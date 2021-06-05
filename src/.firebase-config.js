000d951854fe0980d2d5006ac83433f16e76b4f0
billiejoenahema <billiejoenahema@gmail.com>
2021-06-05 11:30:52 +0900

    install: firebase

---
+++ src/.firebase-config.js
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD8tW-KSQeWvbzdIdZtM8K3iJDORhMUEPY",
+  authDomain: "strength-level-checker.firebaseapp.com",
+  projectId: "strength-level-checker",
+  storageBucket: "strength-level-checker.appspot.com",
+  messagingSenderId: "224086691705",
+  appId: "1:224086691705:web:3d79b20c222044eb9f69a8",
+  measurementId: "G-PR4VQ2LYM9"
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
