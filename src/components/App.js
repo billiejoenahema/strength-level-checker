2d0a362c6fbb32a66a8675306bbcb4ac20a4437d
billiejoenahema <billiejoenahema@gmail.com>
2021-06-05 09:19:54 +0900

    add: state weight&setWeight to App comoponent

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,11 @@ import config from '../config.json'
 
 const App = () => {
   const [name, setName] = useState('')
+  const [weight, setWeight] = useState('')
   return (
     (name === '' && config.signInEnabled) ?
-      <SignIn setName={setName} />
-      : <Main name={name} />
+      <SignIn setName={setName} setWeight={setWeight} />
+      : <Main name={name} weight={weight} />
   )
 }
 
