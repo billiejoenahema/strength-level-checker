8e624b54e0d1449fcbb836e9f2c072f0616f317a
billiejoenahema <billiejoenahema@gmail.com>
2021-06-13 20:01:06 +0900

    add: initialize state value to addReport

--- src/components/ReportSubmitButton.js
+++ src/components/ReportSubmitButton.js
@@ -1,26 +1,29 @@
 import React from 'react'
-import { IconButton } from '@material-ui/core';
-import SendIcon from '@material-ui/icons/Send';
+import { IconButton } from '@material-ui/core'
+import SendIcon from '@material-ui/icons/Send'
 import { addReport } from '../firebase'
 
-const ReportSubmitButton = ({
-  name,
-  bodyWeight,
-  report
-}) => {
+const ReportSubmitButton = ({ report, setReport, user }) => {
   const isInputted = (report.lift === 0) || (report.reps === 0)
   return (
     <IconButton disabled={isInputted} onClick={
       async () => {
         try {
           await addReport({
-            name: name,
-            bodyWeight: bodyWeight,
+            userName: user.userName,
+            bodyWeight: user.bodyWeight,
             exercise: report.exercise,
             lift: report.lift,
             reps: report.reps,
             maxLift: report.maxLift,
             strengthLevel: report.strengthLevel
+          })
+          setReport({
+            ...report,
+            lift: 0,
+            reps: 0,
+            maxLift: 0,
+            strengthLevel: ''
           })
         } catch (error) {
           alert(error.message)
