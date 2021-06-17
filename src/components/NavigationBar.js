3a6cc35a6b8c68f4735d9d3983e3764e18fa426f
billiejoenahema <billiejoenahema@gmail.com>
2021-06-17 21:09:29 +0900

    change: styles Navbar to simple

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import { firebaseApp } from '../firebase.js'
-import { makeStyles, Box } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
-import PersonIcon from '@material-ui/icons/Person'
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Button,
-  IconButton,
-} from '@material-ui/core'
-
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,14 +18,14 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
-}));
+}))
 
 const NavigationBar = ({ userName }) => {
   const classes = useStyles()
 
   return (
     <div className={classes.root}>
-      <AppBar position="fixed">
+      <AppBar position="static">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
@@ -37,18 +33,11 @@ const NavigationBar = ({ userName }) => {
           <Typography variant="h6" className={classes.title}>
             Strength Level Checker
           </Typography>
-          <Box>
-            <Box>
-              <PersonIcon />
-            </Box>
-            <Box>
-              {userName}
-            </Box>
-          </Box>
+          <Button color="inherit">user: {userName}</Button>
           <Button color="inherit" onClick={() => firebaseApp.auth().signOut()}>logout</Button>
         </Toolbar>
       </AppBar>
-    </div >
+    </div>
   )
 }
 
