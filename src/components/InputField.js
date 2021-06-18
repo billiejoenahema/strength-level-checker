12993cf374f88b306daadbeb81df2f63adcffca0
billiejoenahema <billiejoenahema@gmail.com>
2021-06-18 20:11:51 +0900

    style: fixed InputField to bottom

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -24,6 +24,12 @@ const useStyles = makeStyles((theme) => ({
       gridRow: 2,
     },
   },
+  stickToBottom: {
+    width: '100%',
+    position: 'fixed',
+    bottom: 0,
+    background: 'white',
+  },
 }))
 
 const InputField = ({ user, setUser }) => {
@@ -50,79 +56,81 @@ const InputField = ({ user, setUser }) => {
   })
 
   return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <Grid container>
-        <Grid item xs={1}>
-          <Avatar src={avatarPath} />
-          <span>{user.userName}</span>
-        </Grid>
-        <Grid item xs={10}>
-          <FormControl margin="normal">
-            <InputLabel shrink htmlFor="exercise-native-label-placeholder">
-              Exercise
+    <Box Box className={classes.stickToBottom} boxShadow={2} >
+      <form className={classes.root} noValidate autoComplete="off">
+        <Grid container>
+          <Grid item xs={1}>
+            <Avatar src={avatarPath} />
+            <span>{user.userName}</span>
+          </Grid>
+          <Grid item xs={10}>
+            <FormControl margin="normal">
+              <InputLabel shrink htmlFor="exercise-native-label-placeholder">
+                Exercise
             </InputLabel>
-            <NativeSelect
-              value={report.exercise}
-              onChange={(e) => setReport({ ...report, exercise: e.target.value })}
-              inputProps={{
-                name: 'exercise',
-                id: 'exercise-native-label-placeholder',
+              <NativeSelect
+                value={report.exercise}
+                onChange={(e) => setReport({ ...report, exercise: e.target.value })}
+                inputProps={{
+                  name: 'exercise',
+                  id: 'exercise-native-label-placeholder',
+                }}
+              >
+                <option value="benchPress">Bench Press</option>
+              </NativeSelect>
+            </FormControl>
+            <TextField
+              required
+              label="Lifted"
+              id="standard-start-adornment"
+              margin="normal"
+              InputProps={{
+                endAdornment: <InputAdornment position="end">kg</InputAdornment>,
               }}
-            >
-              <option value="benchPress">Bench Press</option>
-            </NativeSelect>
-          </FormControl>
-          <TextField
-            required
-            label="Lifted"
-            id="standard-start-adornment"
-            margin="normal"
-            InputProps={{
-              endAdornment: <InputAdornment position="end">kg</InputAdornment>,
-            }}
-            onChange={(e) => setReport({ ...report, lift: Number(e.target.value) })}
-          />
-          <FormControl margin="normal">
-            <InputLabel shrink htmlFor="reps-native-label-placeholder">
-              Reps
+              onChange={(e) => setReport({ ...report, lift: Number(e.target.value) })}
+            />
+            <FormControl margin="normal">
+              <InputLabel shrink htmlFor="reps-native-label-placeholder">
+                Reps
             </InputLabel>
-            <NativeSelect
-              value={report.reps}
-              onChange={(e) => setReport({ ...report, reps: Number(e.target.value) })}
-              inputProps={{
-                name: 'reps',
-                id: 'reps-native-label-placeholder',
+              <NativeSelect
+                value={report.reps}
+                onChange={(e) => setReport({ ...report, reps: Number(e.target.value) })}
+                inputProps={{
+                  name: 'reps',
+                  id: 'reps-native-label-placeholder',
+                }}
+              >
+                <RepsSelector />
+              </NativeSelect>
+            </FormControl>
+            <TextField
+              id="standard-read-only-input"
+              label="Your Max Weight"
+              value={report.maxLift}
+              InputProps={{
+                readOnly: true,
               }}
-            >
-              <RepsSelector />
-            </NativeSelect>
-          </FormControl>
-          <TextField
-            id="standard-read-only-input"
-            label="Your Max Weight"
-            value={report.maxLift}
-            InputProps={{
-              readOnly: true,
-            }}
-          />
-          <TextField
-            id="standard-read-only-input"
-            label="your Strength Level"
-            value={report.strengthLevel}
-            InputProps={{
-              readOnly: true,
-            }}
-          />
+            />
+            <TextField
+              id="standard-read-only-input"
+              label="your Strength Level"
+              value={report.strengthLevel}
+              InputProps={{
+                readOnly: true,
+              }}
+            />
+          </Grid>
+          <Grid item xs={1}>
+            <ReportSubmitButton
+              report={report}
+              setReport={setReport}
+              user={user}
+            />
+          </Grid>
         </Grid>
-        <Grid item xs={1}>
-          <ReportSubmitButton
-            report={report}
-            setReport={setReport}
-            user={user}
-          />
-        </Grid>
-      </Grid>
-    </form>
+      </form>
+    </Box>
   )
 }
 
