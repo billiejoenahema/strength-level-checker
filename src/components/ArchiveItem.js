2cf8e2b62e34c8dac753b07d73b9f7d58cbc9b13
billiejoenahema <billiejoenahema@gmail.com>
2021-06-20 16:12:22 +0900

    add: delete confirm dialog

--- src/components/ArchiveItem.js
+++ src/components/ArchiveItem.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import PaperComponent from './PaperComponent'
 import { makeStyles } from '@material-ui/core/styles'
 import { gravatarPath } from '../gravatar'
-import { formatDate } from '../formatDate'
 import { deleteReport } from '../firebase'
+import { formatDate } from '../formatDate'
 import DeleteIcon from '@material-ui/icons/Delete'
 import {
   List,
@@ -11,7 +12,13 @@ import {
   ListItemAvatar,
   Avatar,
   Typography,
-  IconButton
+  IconButton,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Button
 } from '@material-ui/core'
 
 const useStyles = makeStyles(() => ({
@@ -24,7 +31,16 @@ const ArchiveItem = ({ archive, setArchives, setIsSubmit }) => {
   const classes = useStyles()
   const avatarPath = gravatarPath(archive.userName)
   const { userName, bodyWeight, lift, reps, maxLift, strengthLevel, created_at, id } = archive
+  const [open, setOpen] = React.useState(false)
   const formattedDate = formatDate(created_at)
+
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
 
   return (
     <ListItem divider={true}>
@@ -75,18 +91,41 @@ const ArchiveItem = ({ archive, setArchives, setIsSubmit }) => {
           </Typography>
         }
       />
-      <IconButton onClick={
-        async () => {
-          try {
-            await deleteReport(id)
-            setArchives([])
-            setIsSubmit(true)
-          } catch (error) {
-            alert(error.message)
-          }
-        }}>
+      <IconButton onClick={handleClickOpen}>
         <DeleteIcon />
       </IconButton>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperComponent={PaperComponent}
+        aria-labelledby="draggable-dialog-title"
+      >
+        <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
+          記録を削除しますか？
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Delete ボタンを押すと記録は完全に消去されます
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button color="primary" onClick={
+            async () => {
+              try {
+                await deleteReport(id)
+                setArchives([])
+                setIsSubmit(true)
+              } catch (error) {
+                alert(error.message)
+              }
+            }}>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
     </ListItem>
   )
 }
