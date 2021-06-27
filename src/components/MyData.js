74f631aa2af4c4a6667da941689fdb029b1c3a8d
billiejoenahema <billiejoenahema@gmail.com>
2021-06-27 12:55:18 +0900

    create: chart by user data

--- src/components/MyData.js
+++ src/components/MyData.js
@@ -1,5 +1,16 @@
 import React from 'react'
 import { makeStyles, Modal, Backdrop, Fade } from '@material-ui/core'
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  // ResponsiveContainer
+} from 'recharts'
+import { formatChartDate } from '../formatChartDate'
 
 const useStyles = makeStyles((theme) => ({
   modal: {
@@ -9,7 +20,6 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     backgroundColor: theme.palette.background.paper,
-    // border: '2px solid #000',
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
@@ -17,8 +27,14 @@ const useStyles = makeStyles((theme) => ({
 
 const MyData = ({ archives, open, setOpen }) => {
   const classes = useStyles()
-  const maxLiftList = archives.map((archive) => {
-    return archive.maxLift
+
+  archives.forEach((archive) => {
+    // m/dの形にフォーマットしたdateをarchivesに追加する
+    archive['date'] = formatChartDate(archive.created_at)
+  })
+
+  const data = archives.filter((item) => {
+    return item.exercise === 'ベンチプレス'
   })
 
   return (
@@ -37,7 +53,24 @@ const MyData = ({ archives, open, setOpen }) => {
       <Fade in={open}>
         <div className={classes.paper}>
           <h2 id="transition-modal-title">My Data</h2>
-          <p id="transition-modal-description">{maxLiftList}</p>
+          <LineChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="maxLift" stroke="#8884d8" activeDot={{ r: 8 }} />
+          </LineChart>
         </div>
       </Fade>
     </Modal>
