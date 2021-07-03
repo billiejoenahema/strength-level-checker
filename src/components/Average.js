120c762107d3def5059475892ed8c06e83906f75
billiejoenahema <billiejoenahema@gmail.com>
2021-07-03 15:50:38 +0900

    remove: dead code

--- src/components/Average.js
+++ src/components/Average.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Typography } from '@material-ui/core'
 
 const Average = ({ records, chart }) => {
-  console.log(records)
   // 選択された種目のmaxLiftだけを抽出して配列を作る
   const maxLifts = records.map((item) => {
-    if (item.exercise === chart) return item.maxLift
+    if (item.exercise === chart) { return item.maxLift }
+    return
   })
   const dataAverage = maxLifts.reduce((a, b) => a + b) / maxLifts.length
 
