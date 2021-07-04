0be6a656b92ae85f6ec6c23bcf9b2efb58726417
billiejoenahema <billiejoenahema@gmail.com>
2021-07-04 20:10:08 +0900

    remove: dead code

--- src/components/HideOnScroll.js
+++ src/components/HideOnScroll.js
@@ -4,6 +4,7 @@ import { useScrollTrigger, Slide } from '@material-ui/core'
 const HideOnScroll = ({ children }) => {
   const trigger = useScrollTrigger()
   // ボトムのinputFieldはスクロールで下へフレームアウトさせる
+  console.log(children.props.id)
   const direction = (children.props.id === 'stick-bottom')
     ? 'up' : 'down'
 
