4cb8508eec0b53bb06f4dc4965785b7f058e574a
billiejoenahema <billiejoenahema@gmail.com>
2021-07-04 20:10:57 +0900

    remove: unnecessary div

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -61,60 +61,58 @@ const InputField = ({ user, setIsSubmit, setArchives }) => {
 
   return (
     <HideOnScroll>
-      <div className={classes.root}>
-        <Box className={classes.stickToBottom} boxShadow={2} id="stick-bottom" >
-          <form noValidate autoComplete="off">
-            <Grid container alignItems="center">
-              <Grid item xs={1}>
-                <Avatar src={avatarPath} className={classes.avatar} />
-                <Typography align="center" >{user.userName}</Typography>
-              </Grid>
-              <Grid item xs={10}>
-                <ExerciseSelector report={report} setReport={setReport} />
-                {/* input lift */}
-                <TextField
-                  id="lift-input-adornment"
-                  label="挙上重量"
-                  InputProps={{
-                    endAdornment: <InputAdornment position="end">kg</InputAdornment>,
-                  }}
-                  onFocus={(e) => { e.target.value = '' }}
-                  onChange={(e) => setReport({ ...report, lift: Number(e.target.value) })}
-                />
-                {/* select reps */}
-                <RepsSelector report={report} setReport={setReport} />
-                {/* display max lift */}
-                <TextField
-                  id="max-lift-input"
-                  label="最大挙上重量"
-                  value={report.maxLift}
-                  InputProps={{
-                    readOnly: true,
-                  }}
-                />
-                {/* display strength level */}
-                <TextField
-                  id="strength-level-input"
-                  label="称号"
-                  value={report.strengthLevel}
-                  InputProps={{
-                    readOnly: true,
-                  }}
-                />
-              </Grid>
-              <Grid item xs={1}>
-                <ReportSubmitButton
-                  report={report}
-                  setReport={setReport}
-                  user={user}
-                  setIsSubmit={setIsSubmit}
-                  setArchives={setArchives}
-                />
-              </Grid>
+      <Box className={classes.stickToBottom} boxShadow={2} id="stick-bottom" >
+        <form className={classes.root} noValidate autoComplete="off">
+          <Grid container alignItems="center">
+            <Grid item xs={1}>
+              <Avatar src={avatarPath} className={classes.avatar} />
+              <Typography align="center" >{user.userName}</Typography>
             </Grid>
-          </form>
-        </Box>
-      </div>
+            <Grid item xs={10}>
+              <ExerciseSelector report={report} setReport={setReport} />
+              {/* input lift */}
+              <TextField
+                id="lift-input-adornment"
+                label="挙上重量"
+                InputProps={{
+                  endAdornment: <InputAdornment position="end">kg</InputAdornment>,
+                }}
+                onFocus={(e) => { e.target.value = '' }}
+                onChange={(e) => setReport({ ...report, lift: Number(e.target.value) })}
+              />
+              {/* select reps */}
+              <RepsSelector report={report} setReport={setReport} />
+              {/* display max lift */}
+              <TextField
+                id="max-lift-input"
+                label="最大挙上重量"
+                value={report.maxLift}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+              {/* display strength level */}
+              <TextField
+                id="strength-level-input"
+                label="称号"
+                value={report.strengthLevel}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+            </Grid>
+            <Grid item xs={1}>
+              <ReportSubmitButton
+                report={report}
+                setReport={setReport}
+                user={user}
+                setIsSubmit={setIsSubmit}
+                setArchives={setArchives}
+              />
+            </Grid>
+          </Grid>
+        </form>
+      </Box>
     </HideOnScroll>
   )
 }
